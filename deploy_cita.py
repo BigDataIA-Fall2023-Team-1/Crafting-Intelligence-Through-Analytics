00f22c69198b65d60d39d8eb056ad542a04f3b76
PrathamHusky07 <kulkarni.prath@northeastern.edu>
2023-12-13 19:40:38 -0500

    Changes in the CI/Cd file

--- deploy_cita.py
+++ deploy_cita.py
@@ -12,7 +12,7 @@ print(f"Deploying LinkedIn / Indeed Jobs in root directory {root_directory}")
 # For example, you can invoke your existing deployment script or commands
 
 # Change directory to your Airflow DAG directory
-dag_directory = os.path.join(root_directory, 'airflow/dags/test1.py')
+dag_directory = os.path.join(root_directory, 'airflow/dags')
 os.chdir(dag_directory)
 
 # Run your Airflow DAG deployment command
